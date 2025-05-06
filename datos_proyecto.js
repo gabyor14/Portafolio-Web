78c0344555f0f8a8675a2405545610813ce52706
gabyor14 <ortizgabycsa@gmail.com>
2025-05-05 22:48:24 -0600

    Add files via upload

---
+++ datos_proyecto.js
@@ -0,0 +1,13 @@
+
+
+
+const menuBtn = document.querySelector(".header .menu-btn");
+const menu = document.querySelector(".header .menu");
+
+
+function toggleMenu(){
+	menuBtn.classList.toggle("active");
+	menu.classList.toggle("open");
+}
+
+menuBtn.addEventListener("click", toggleMenu);
